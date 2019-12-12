f7f97af316d0477efe84a3941567bc088c414737
SeaklimLay <58453719+SeaklimLay@users.noreply.github.com>
2019-12-12 09:53:56 +0700

    [ADD] 613_upd_helpdesk_ext_qtl (#22)
    * [ADD] 613_upd_helpdesk_ext_qtl
    
    * [ADD] 613_upd_helpdesk_ext_qtl

--- helpdesk_ext_qtl/models/helpdesk_team.py
+++ helpdesk_ext_qtl/models/helpdesk_team.py
@@ -1,13 +1,12 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class HelpdeskTeam(models.Model):
-    _inherit = 'helpdesk.team'
+    _inherit = "helpdesk.team"
 
     customer_reply_stage_id = fields.Many2one(
-        'helpdesk.stage',
-        string='Customer\'s Update Stage',
+        "helpdesk.stage", string="Customer's Update Stage"
     )
--- helpdesk_ext_qtl/models/helpdesk_ticket.py
+++ helpdesk_ext_qtl/models/helpdesk_ticket.py
@@ -1,13 +1,10 @@
 # Copyright 2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
+    _inherit = "helpdesk.ticket"
 
-    ticket_deadline = fields.Date(
-        string='Deadline',
-        copy=False,
-    )
+    ticket_deadline = fields.Date(string="Deadline", copy=False)
