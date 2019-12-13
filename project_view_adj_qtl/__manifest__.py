4752b9f3e3498588b7593494953a73b6f2164878
Yoshi Tashiro <tashiro@quartile.co>
2019-12-13 23:47:52 +0900

    Add project view adj qtl (#27)
    * add project_view_adj_qtl
    
    * update pre commit config
    
    * upd isort, black
    
    * remove readme

---
+++ project_view_adj_qtl/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018-2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Project View Adj",
+    "version": "12.0.1.0.0",
+    "depends": ["project_stage_state"],
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "website": "https://www.quartile.co",
+    "category": "Project",
+    "description": """
+    """,
+    "data": ["views/project_task_views.xml"],
+    "installable": True,
+}
