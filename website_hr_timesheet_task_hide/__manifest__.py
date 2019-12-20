3204d44614b7be9394f903d781da121263a77084
Tim <tl@quartile.co>
2019-12-20 14:22:43 +0800

    [IMP] Project tasks feature adjustments (#31)

---
+++ website_hr_timesheet_task_hide/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Hide timesheet task in portal page",
+    "version": "12.0.1.0.0",
+    "depends": ["hr_timesheet"],
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "website": "https://www.quartile.co",
+    "category": "HR",
+    "description": """
+    """,
+    "data": ["views/hr_timesheet_views.xml"],
+    "installable": True,
+}
