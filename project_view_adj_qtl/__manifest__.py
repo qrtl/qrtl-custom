84c7022935178bc98ce11c7beb8077925de6eb22
Yoshi Tashiro <tashiro@quartile.co>
2019-12-21 23:51:44 +0900

    set default_order on task tree, show display_name in task kanban (#35)

--- project_view_adj_qtl/__manifest__.py
+++ project_view_adj_qtl/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project View Adj",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "depends": ["project_stage_state"],
     "author": "Quartile Limited",
     "license": "AGPL-3",
