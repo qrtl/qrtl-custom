43895b39ced9ecbd25edb28b1ab695fb7890b5c4
Yoshi Tashiro <tashiro@quartile.co>
2020-03-11 14:53:51 +0900

    [FIX] project view adj qtl, add README files (#78)
    * [FIX] project_view_adj_qtl: remove field references
    
    date_start and date_end fields are added to the form view in project_timeline_module.
    Instead of adding the dependency, we will remove the references to these fields for
    ease of maintenance.
    
    * [IMP] .pre-commit-config.yaml: remove opensource modules
    
    * [IMP] website_forum_post_access: add README.rst
    
    * [FIX] website_forum_post_access: remove undefined badges
    
    * [IMP] website_crm_form_adj: add README.rst

--- project_view_adj_qtl/__manifest__.py
+++ project_view_adj_qtl/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2018-2019 Quartile Limited
+# Copyright 2018-2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project View Adj",
-    "version": "12.0.1.1.1",
+    "version": "12.0.1.1.2",
     "depends": ["project_stage_state"],
     "author": "Quartile Limited",
     "license": "AGPL-3",
