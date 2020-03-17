91a9a22629105c30316cf3d78348e7c7396202aa
Yoshi Tashiro <tashiro@quartile.co>
2020-03-17 12:37:08 +0800

    [ADD] analytic_group_sequence

---
+++ analytic_group_sequence/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Analytic Group Sequence",
+    "version": "12.0.1.0.0",
+    "depends": ["analytic"],
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "website": "https://www.quartile.co",
+    "category": "Accounting",
+    "data": ["views/account_analytic_group_views.xml"],
+    "installable": True,
+}
