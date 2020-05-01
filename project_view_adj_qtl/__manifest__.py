9de1fe72d8442a58b75ba46efbcc482de8c32df8
Agano oasis <oasisagano@gmail.com>
2020-05-01 09:40:24 +0900

    Update version in manifest
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>

--- project_view_adj_qtl/__manifest__.py
+++ project_view_adj_qtl/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project View Adj",
-    "version": "12.0.1.1.2",
+    "version": "12.0.1.2.0",
     "depends": ["timesheet_billability", "project_stage_state"],
     "author": "Quartile Limited",
     "license": "AGPL-3",
