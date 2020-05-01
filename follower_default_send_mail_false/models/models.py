12c4d0328580bbbe4c59c2a21607829cd8db2562
Agano oasis <oasisagano@gmail.com>
2020-05-01 16:24:49 +0900

    Update follower_default_send_mail_false/models/models.py
    Co-authored-by: Yoshi Tashiro <tashiro@quartile.co>

--- follower_default_send_mail_false/models/models.py
+++ follower_default_send_mail_false/models/models.py
@@ -8,7 +8,6 @@ class Invite(models.TransientModel):
     _inherit = "mail.wizard.invite"
 
     send_mail = fields.Boolean(
-        "Send Email",
         default=False,
         help="If checked, the partners will receive an \
             email warning they have been added in the document's followers.",
