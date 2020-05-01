636edf5dd0cf647b59c91ed0b27259512fa4aeb0
kenseii <oasisagano@gmail.com>
2020-05-01 14:06:03 +0800

    [FIX] fixed the code formatting for precommit

--- follower_default_send_mail_false/models/models.py
+++ follower_default_send_mail_false/models/models.py
@@ -1,8 +1,12 @@
-# -*- coding: utf-8 -*-
+from odoo import fields, models
 
-from odoo import models, fields, api
 
 class Invite(models.TransientModel):
-    _inherit="mail.wizard.invite"
+    _inherit = "mail.wizard.invite"
 
-    send_mail = fields.Boolean('Send Email', default=False, help="If checked, the partners will receive an email warning they have been added in the document's followers.")
+    send_mail = fields.Boolean(
+        "Send Email",
+        default=False,
+        help="If checked, the partners will receive an \
+            email warning they have been added in the document's followers.",
+    )
