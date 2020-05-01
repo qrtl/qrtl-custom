5981de6ab3274a9c53f8c3c2103c9fc460434b2d
kenseii <oasisagano@gmail.com>
2020-05-01 16:22:26 +0800

    fixed the changes from the feedback

--- follower_default_send_mail_false/models/models.py
+++ follower_default_send_mail_false/models/models.py
@@ -7,6 +7,4 @@ from odoo import fields, models
 class Invite(models.TransientModel):
     _inherit = "mail.wizard.invite"
 
-    send_mail = fields.Boolean(
-        default=False,
-    )
+    send_mail = fields.Boolean(default=False,)
