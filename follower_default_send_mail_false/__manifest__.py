3a0f85a4ebb34698d0cee0b3a2ac878724f8f1e5
kenseii <oasisagano@gmail.com>
2020-05-01 14:06:03 +0800

    [FIX] fixed the code formatting for precommit

--- follower_default_send_mail_false/__manifest__.py
+++ follower_default_send_mail_false/__manifest__.py
@@ -1,35 +1,15 @@
-# -*- coding: utf-8 -*-
+# Copyright 2019-2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "follower_default_send_mail_false",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+    "name": "follower_default_send_mail_false",
+    "summary": """
+        Sets the send_mail field to False by default
+        when adding a new follower
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base','mail'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "author": "Quartile Limited, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Uncategorized",
+    "version": "12.0.0.0.1",
+    "depends": ["base", "mail"],
+    "data": ["views/views.xml", "views/templates.xml"],
 }
