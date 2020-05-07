11b0de9519a7539a3eebbc4dedf7cff999671eab
kenseii <oasisagano@gmail.com>
2020-05-07 09:33:25 +0800

    renamed the module, updated the version and removed useless file

--- follower_default_send_mail_false/__manifest__.py
+++ follower_default_send_mail_false/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "follower_default_send_mail_false",
+    "name": "mail_send_mail_default_false",
     "summary": """
         Sets the send_mail field to False by default
         when adding a new follower
     """,
     "author": "Quartile Limited, " "Odoo Community Association (OCA)",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "category": "Uncategorized",
-    "version": "12.0.0.0.1",
-    "depends": ["base", "mail"],
+    "version": "12.0.1.0.0",
+    "depends": ["mail"],
     "data": [],
 }
