1eb01a590d9b9da60472e07e0936477fa8ff18e0
kenseii <oasisagano@gmail.com>
2020-05-08 13:30:41 +0800

    [FIX] updated the read me and manifest to use the correct names

--- follower_default_send_mail_false/__manifest__.py
+++ follower_default_send_mail_false/__manifest__.py
@@ -1,15 +1,14 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "mail_send_mail_default_false",
+    "name": "Send Mail Default False",
     "summary": """
         Sets the send_mail field to False by default
         when adding a new follower
     """,
-    "author": "Quartile Limited, " "Odoo Community Association (OCA)",
+    "author": "Quartile Limited, ",
     "license": "LGPL-3",
     "category": "Uncategorized",
     "version": "12.0.1.0.0",
     "depends": ["mail"],
-    "data": [],
 }
