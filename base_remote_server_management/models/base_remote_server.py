9f7500aaf7311b6212852d1f4edca2365fb2f5b3
tslai <tl@quartile.co>
2020-11-05 17:04:59 +0800

    [ADD] base_remote_server_management

---
+++ base_remote_server_management/models/base_remote_server.py
@@ -0,0 +1,23 @@
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, fields
+
+
+class BaseRemoteServer(models.Model):
+    _name = 'base.remote.server'
+
+    name = fields.Char("Description/Usage", required=True)
+    server_ip = fields.Char("IP", required=True)
+    ssh_user = fields.Char("SSH User")
+    ssh_port = fields.Integer("SSH Port Number")
+    private_key_path = fields.Char("Private Key Path", required=True)
+    active = fields.Boolean("Active", default=True)
+    server_command_properties_ids = fields.One2many('server.command.properties', 'base_remote_server_id', string='Command Properties')
+    server_static_properties_ids = fields.One2many('server.static.properties', 'base_remote_server_id', string='Static Properties')
+    server_url_ids = fields.One2many('base.remote.url', 'base_remote_server_id', string='Access URL(s)')
+    database_ids = fields.One2many('base.remote.database', 'base_remote_server_id', string='Odoo Database(s)')
+
+    def sync_custom_properties(self):
+        for prop in self.server_command_properties_ids:
+            prop._compute_property_value()
