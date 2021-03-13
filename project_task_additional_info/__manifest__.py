cbbabf221594d2e0cc095bc3f6861d16b659f43b
Le Hoang An <anlh@trobz.com>
2021-03-13 15:58:07 +0700

    [1703][ADD] base_additional_info,project_task_additional_info

--- project_task_additional_info/__manifest__.py
+++ project_task_additional_info/__manifest__.py
@@ -3,11 +3,11 @@
 
 {
     "name": "Project task additional info",
-    "version": "12.0.1.0.0",
-    "author": "QUARTILE LIMITED",
-    "website": "https://www.quartile.co/",
     "category": "Usability",
     "license": "LGPL-3",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "version": "12.0.1.0.1",
     "depends": ["base_additional_info", "project"],
     "data": ["security/ir.model.access.csv", "views/project_task_view.xml"],
     "installable": True,
