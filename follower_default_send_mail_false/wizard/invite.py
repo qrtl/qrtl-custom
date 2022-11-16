e2168017488e049e52f6f1d5a9c037b4bfabe241
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-11-16 10:41:21 +0630

    [IMP] follower_default_send_mail_false: black, isort, prettier

--- follower_default_send_mail_false/wizard/invite.py
+++ follower_default_send_mail_false/wizard/invite.py
@@ -7,4 +7,6 @@ from odoo import fields, models
 class Invite(models.TransientModel):
     _inherit = "mail.wizard.invite"
 
-    send_mail = fields.Boolean(default=False,)
+    send_mail = fields.Boolean(
+        default=False,
+    )
