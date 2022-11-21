6b4e8af95aa10a400ae764064fe9425acb99df56
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-11-21 09:03:36 +0630

    [2227][MIG] helpdesk_ext_qtl: Migration to 16.0

--- helpdesk_ext_qtl/__manifest__.py
+++ helpdesk_ext_qtl/__manifest__.py
@@ -1,14 +1,13 @@
 # Copyright 2019 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Extension for Helpdesk functions",
     "category": "Helpdesk",
-    "version": "12.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Quartile Limited",
     "website": "https://www.quartile.co",
     "license": "AGPL-3",
     "depends": ["helpdesk"],
-    "summary": """""",
     "data": ["views/helpdesk_team_views.xml", "views/helpdesk_ticket_views.xml"],
     "installable": True,
 }
--- helpdesk_ext_qtl/models/mail_message.py
+++ helpdesk_ext_qtl/models/mail_message.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
 
@@ -7,20 +7,23 @@ from odoo import api, models
 class MailMessage(models.Model):
     _inherit = "mail.message"
 
-    @api.model
-    def create(self, vals):
-        res = super(MailMessage, self).create(vals)
-        if (
-            res.model == "helpdesk.ticket"
-            and res.subtype_id
-            and not res.subtype_id.internal
-        ):
-            ticket = self.env["helpdesk.ticket"].browse(res.res_id)
-            if ticket.team_id and ticket.stage_id.sequence != 0:
-                team_members = ticket.team_id.member_ids.mapped("partner_id")
-                if (
-                    res.author_id not in team_members
-                    and ticket.team_id.customer_reply_stage_id
-                ):
-                    ticket.stage_id = ticket.team_id.customer_reply_stage_id
-        return res
+    @api.model_create_multi
+    def create(self, values_list):
+        messages = super(MailMessage, self).create(values_list)
+        for message in messages:
+            if not (
+                message.model == "helpdesk.ticket"
+                and message.subtype_id
+                and not message.subtype_id.internal
+            ):
+                continue
+            ticket = self.env["helpdesk.ticket"].browse(message.res_id)
+            if not (ticket.team_id and ticket.stage_id.sequence != 0):
+                continue
+            team_members = ticket.team_id.member_ids.mapped("partner_id")
+            if (
+                message.author_id not in team_members
+                and ticket.team_id.customer_reply_stage_id
+            ):
+                ticket.stage_id = ticket.team_id.customer_reply_stage_id
+        return messages
