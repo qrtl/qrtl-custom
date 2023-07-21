5ab7b53633a7f0307650da1fd220a1522ee8999d
Yoshi Tashiro <tashiro@quartile.co>
2023-07-21 23:14:52 +0800

    [FIX] project_view_adj_qtl: remove dependency on timesheet_billability

--- project_view_adj_qtl/__manifest__.py
+++ project_view_adj_qtl/__manifest__.py
@@ -2,8 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project View Adj",
-    "version": "12.0.1.2.0",
-    "depends": ["timesheet_billability", "project_stage_state"],
+    "version": "12.0.1.3.0",
+    "depends": ["project_stage_state"],
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "website": "https://www.quartile.co",
