3dfa1f275bb47efe39b5ac76d94eba6d903f6947
Yoshi Tashiro <tashiro@quartile.co>
2023-09-09 03:50:15 +0000

    [3685][FIX] hr_timesheet_unpropagate_project: _compute_project_id()
    Before this commit, the issue of project propagation from updating task's project
    was still happening when user opens the task form via timesheet records.
    
    This commit fixes the issue by not relying on the context value.

--- hr_timesheet_unpropagate_project/models/hr_timesheet.py
+++ hr_timesheet_unpropagate_project/models/hr_timesheet.py
@@ -8,9 +8,9 @@ class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     def _compute_project_id(self):
-        model = self._context.get("params", {}).get("model")
-        if model and model == "project.task":
-            # Avoid updating the project of existing timesheet records when project
-            # assignment is changed in the task.
-            return
-        super()._compute_project_id()
+        # Avoid updating the project of existing timesheet records which are already
+        # validated or not owned by the current user.
+        self = self.filtered(
+            lambda x: not x.validated and x.employee_id.user_id == self.env.user
+        )
+        return super()._compute_project_id()
