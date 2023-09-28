e72b1fe6574251a23ab21415bcfa9cdd2c38473a
Aungkokolin1997 <lin@quartile.co>
2023-09-28 11:37:20 +0630

    [ADD] project_task_follower_validation

---
+++ project_task_follower_validation/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Project Task Follower Validation",
+    "version": "16.0.1.0.0",
+    "depends": ["project"],
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "website": "https://www.quartile.co",
+    "category": "project",
+    "data": ["views/project_task_views.xml"],
+    "installable": True,
+}
