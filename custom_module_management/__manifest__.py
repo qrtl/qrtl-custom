9f0197a8fe0edcb7cece68308785cf155878db50
Aungkokolin1997 <lin@quartile.co>
2024-05-29 05:43:44 +0000

    [ADD] custom_module_management

---
+++ custom_module_management/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Custom Module Management",
+    "version": "16.0.1.0.0",
+    "category": "Subscription",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "depends": ["sale", "product_odoo_module"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_product_views.xml",
+        "views/customer_info_views.xml",
+    ],
+}
---
+++ custom_module_management/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module is designed to add customer information to the Odoo module
+product.
