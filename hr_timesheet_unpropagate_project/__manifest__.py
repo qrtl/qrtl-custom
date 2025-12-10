39bced5b2c1cd2f3b56fde42fa8ddd1f6f4a25cb
Aungkokolin1997 <lin@quartile.co>
2025-12-10 07:37:58 +0000

    [MIG] hr_timesheet_unpropagate_project: Migration to 19.0

--- hr_timesheet_unpropagate_project/__manifest__.py
+++ hr_timesheet_unpropagate_project/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2023 Quartile Limited
+# Copyright 2023 Quartile (https:/www.quartile.co)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "HR Timesheet Unpropagate Project",
     "category": "Timesheet",
-    "version": "16.0.1.0.0",
-    "author": "Quartile Limited",
+    "version": "19.0.1.0.0",
+    "author": "Quartile",
     "website": "https://www.quartile.co",
     "license": "AGPL-3",
     "depends": ["hr_timesheet"],
